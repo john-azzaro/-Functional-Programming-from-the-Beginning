644555f6e313feaaa9fe1e0953ffe77a61e3fb6c
John Azzaro <jazzaro@temple.edu>
2019-12-23 20:41:05 -0800

    updated

--- closureExample.js
+++ closureExample.js
@@ -1,1 +1,14 @@
 "use strict";
+
+function counter() {
+  let count = 0;
+  return function increment() {
+    return ++count;
+  };
+}
+
+let doCount = counter();
+
+console.log(doCount());          // 1
+console.log(doCount());          // 2
+console.log(doCount());          // 3
